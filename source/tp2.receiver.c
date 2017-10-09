e58f17109b496ae842282fe3d290cb5b69167d5f
ineeve <renatoineeve@gmail.com>
2017-10-09 12:33:03 +0100

    ISTO DA MERDA

--- source/tp2.receiver.c
+++ source/tp2.receiver.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> /* To use read function */
+#include <unistd.h>
 
 #define BAUDRATE B38400
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
@@ -16,24 +16,19 @@
 #define C_SET 0x03
 #define C_UA 0x07
 
-/* Global unsigned acknowledge array */
-unsigned char g_ua[5];
+enum read_state {
+	initial,
+	data,
+	finish
+};
 
+int llopen(int fd);
+int llread(int fd, char *data);
 
 int main(int argc, char** argv){
-	int state = 0;
-	unsigned char c;
-	unsigned char package_received[5];
-	int fd,res;
-    struct termios oldtio,newtio;
-    g_ua[0] = FLAG;
-	g_ua[1] = A;
-	g_ua[2] = C_UA;
-	g_ua[3] = g_ua[1]^g_ua[2];
-	g_ua[4] = FLAG;
-
-    char serialName[255] = "/dev/ttyS";
-    if(argv[1]!= 0) strcat(serialName, argv[1]);
+  struct termios oldtio, newtio;
+  char serialName[255] = "/dev/ttyS";
+    if(argv[1] != 0) strcat(serialName, argv[1]);
 
     if ( (argc < 2) ||
   	     (strcmp("/dev/ttyS0", serialName) &&
@@ -42,14 +37,11 @@ int main(int argc, char** argv){
       exit(1);
     }
 
-
-  /*
+   /*
     Open serial port device for reading and writing and not as controlling tty
     because we don't want to get killed if linenoise sends CTRL-C.
-  */
-
-
-    fd = open(serialName, O_RDWR | O_NOCTTY );
+   */
+    int fd = open(serialName, O_RDWR | O_NOCTTY );
     if (fd <0) {perror(serialName); exit(-1); }
 
     if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
@@ -65,17 +57,8 @@ int main(int argc, char** argv){
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
-
-
-
-  /*
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
-    leitura do(s) pr�ximo(s) caracter(es)
-  */
-
-
+    newtio.c_cc[VTIME]    = 0;   /* read time-out */
+    newtio.c_cc[VMIN]     = 1;   /* number chars to read (blocking) */
 
     tcflush(fd, TCIOFLUSH);
 
@@ -84,8 +67,22 @@ int main(int argc, char** argv){
       exit(-1);
     }
    	printf("Waiting for some read\n");
-	
-	
+
+		char* trama = malloc(1024);
+		llopen(fd);
+		llread(fd, trama);
+
+		return 0;
+}
+
+int llopen(int fd) {
+	int state = 0;
+	int res;
+	unsigned char c;
+	unsigned char package_received[5];
+
+	printf("Receiving SET\n");
+
 	while(state != 5){
 
 		res = read(fd,&c,1);
@@ -137,11 +134,64 @@ int main(int argc, char** argv){
 			}
 		}
 	}
-	printf("Received SET\n");
-	printf("Sending acknowledge\n");
-	res = write(fd,g_ua,5);
-	printf("%d bytes written\n",res);
-	
-	return 0;
 
+	unsigned char g_ua[5];
+	g_ua[0] = FLAG;
+	g_ua[1] = A;
+	g_ua[2] = C_UA;
+	g_ua[3] = g_ua[1]^g_ua[2];
+	g_ua[4] = FLAG;
+
+	printf("Received SET\n");
+	printf("Sending UA\n");
+	res = write(fd, g_ua, 5);
+	printf("%d bytes written\n",res);
+
+	return 0;
+}
+
+int llread(int fd, char* trama) {
+	char ch;
+	int result;
+
+	//Read the whole trama
+	printf("Started reading trama\n");
+	int index = 0;
+	int found_flag = 0;
+	while(found_flag < 2) {
+		result = read(fd, &ch, 1);
+		if(result == 1){
+			trama[index++] = ch;
+			printf("Char: %x\n", ch);
+			if(ch == FLAG)
+				found_flag++;
+		} else {
+			printf("Failed to read\n");
+		}
+	}
+	printf("Finished reading trama\n");
+
+	//Check BCC1
+	char address = trama[1];
+	char controll = trama[2];
+	char bcc1 = trama[3];
+	if((address^controll) != bcc1){
+		perror("BCC1 Incorrect parity");
+		return -1;
+	}
+
+	//Check BCC2 and unstuff
+	int size = index;
+	char expected_bcc2 = trama[size - 2];
+	char calculated_bcc2 = 0;
+	int i;
+	for(i = 4; i < size - 2; i++) {
+		calculated_bcc2 ^= trama[i];
+	}
+	if(expected_bcc2 != calculated_bcc2){
+		perror("BCC2 Incorrect parity");
+		return -1;
+	}
+
+	return 0;
 }
