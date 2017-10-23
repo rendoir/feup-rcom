21d0aca3121993c0cc7d2f668bc51b042de15c25
ineeve <renatoineeve@gmail.com>
2017-10-23 18:56:41 +0100

    Added build control frame and build data frame

--- source/linkLayer.c
+++ source/linkLayer.c
@@ -1,55 +1,123 @@
-#include <stdlib.h>
-#include "macros.h"
+#include "linkLayer.h"
 
-int main(){
+int main()
+{
   return 0;
 }
 
-void insertValueAt(char value, char* array, int index, int *array_size){
+long sequence_number = 0;
+
+unsigned char getAddress(int caller, char control_field)
+{
+  if (control_field == C_SET || control_field == C_DISC)
+  {
+    if (caller == SENDER)
+    {
+      return A_SENDER_COMMAND;
+    }
+    return A_RECEIVER_COMMAND;
+  }
+  else
+  {
+    if (caller == RECEIVER)
+    {
+      return A_RECEIVER_COMMAND;
+    }
+    return A_SENDER_COMMAND;
+  }
+}
+
+unsigned char getBCC2(char *data, int data_size)
+{
+  unsigned char bcc = 0;
   int i;
-  for (i = (*array_size) -1; i > index; i--){
-    array[i+1] = array[i];
+  for (i = 0; i < data_size; i++)
+  {
+    bcc = bcc ^ data[i];
+  }
+  return bcc;
+}
+
+void buildControlFrame(char *frame, int caller, char control_field)
+{
+  frame[0] = FLAG;
+  frame[1] = getAddress(caller, control_field);
+  frame[2] = control_field;
+  frame[3] = frame[1] ^ frame[2];
+  frame[4] = FLAG;
+}
+
+void buildDataFrame(char **frame, char *data, int data_size, int *frame_size)
+{
+  *frame_size = data_size + 6;
+  (*frame) = (char *)malloc(*frame_size * sizeof(char));
+  (*frame)[0] = FLAG;
+  (*frame)[1] = A_SENDER_COMMAND;
+  (*frame)[2] = ((sequence_number++) % 2) << 6;
+  (*frame)[3] = (*frame)[1] ^ (*frame)[2];
+  memcpy(&((*frame)[4]), data, data_size);
+  (*frame)[4 + data_size] = getBCC2(data, data_size);
+  (*frame)[5 + data_size] = FLAG;
+  byteStuffing(frame, frame_size);
+}
+
+void byteStuffing(char **frame, int *frame_size)
+{
+  int i;
+  int allocated_space = *frame_size;
+  for (i = 1; i < *frame_size - 1; i++)
+  {
+    char currentByte = (*frame)[i];
+    if (currentByte == FLAG || currentByte == ESCAPE)
+    {
+      (*frame)[i] = currentByte ^ STUFF_XOR;
+      if (allocated_space <= *frame_size)
+      {
+        allocated_space = 2 * allocated_space;
+        realloc(*frame, allocated_space * sizeof(char));
+      }
+      insertValueAt(ESCAPE, *frame, i, frame_size);
+      i++;
+    }
+  }
+}
+
+void byteUnstuffing(char **frame, int *frame_size)
+{
+  int i;
+  for (i = 0; i < *frame_size; i++)
+  {
+    if ((*frame)[i] == ESCAPE)
+    {
+      char byte_xored = (*frame)[i + 1] ^ STUFF_XOR;
+      if (byte_xored == FLAG || byte_xored == ESCAPE)
+      {
+        removeValueAt(*frame, i, frame_size);
+        (*frame)[i] = byte_xored;
+      }
+    }
+  }
+  // Realloc to free unused memory
+  realloc(*frame, *frame_size * sizeof(char));
+}
+
+void insertValueAt(char value, char *array, int index, int *array_size)
+{
+  int i;
+  for (i = (*array_size) - 1; i > index; i--)
+  {
+    array[i + 1] = array[i];
   }
   array[index] = value;
   (*array_size)++;
 }
 
-void removeValueAt(char* array, int index, int *array_size){
+void removeValueAt(char *array, int index, int *array_size)
+{
   int i;
-  for (i = index; i < (*array_size) -1; i++){
-    array[i] = array[i+1];
+  for (i = index; i < (*array_size) - 1; i++)
+  {
+    array[i] = array[i + 1];
   }
   (*array_size)--;
 }
-
-void byteStuffing(char** data, int* data_size){
-   int i;
-   int allocated_space = *data_size;
-   for (i = 0; i < *data_size; i++){
-     char currentByte = (*data)[i];
-     if (currentByte == FLAG || currentByte == ESCAPE){
-       (*data)[i] = currentByte ^ STUFF_XOR;
-       if (allocated_space <= *data_size){
-         allocated_space = 2 * allocated_space;
-         realloc(*data, allocated_space * sizeof(char));
-       }
-       insertValueAt(ESCAPE,*data,i,*data_size);
-       i++;
-     }
-   }
-}
-
- void byteUnstuffing(char** data, int* data_size){
-   int i;
-   for (i = 0; i < *data_size; i++){
-     if ((*data)[i] == ESCAPE){
-       char byte_xored = (*data)[i+1] ^ STUFF_XOR;
-       if (byte_xored == FLAG || byte_xored == ESCAPE){
-         removeValueAt(*data,i,data_size);
-         (*data)[i] = byte_xored;
-       }
-     }
-   }
-   // Realloc to free unused memory
-   realloc(*data, *data_size * sizeof(char));
- }
--- source/linkLayer.h
+++ source/linkLayer.h
@@ -1,7 +1,8 @@
-#ifdef LINK_LAYER_H
+#ifndef LINK_LAYER_H
 #define LINK_LAYER_H
 
 #include <stdlib.h>
+#include <string.h> //used for memcpy
 #include "macros.h"
 
 /**
@@ -12,51 +13,49 @@ void alarm_handler();
 /**
 * Builds a control packet and returns it on the frame parameter.
 * Frame memory should be allocated previously.
+* Caller is usued to select the address value.
 */
-void buildControlFrame(char* frame, char address_field, char control_field);
+void buildControlFrame(char *frame, int caller, char control_field);
 
 /**
 * Creates an Information Frame.
+* It allocates space for the frame.
+* Invokes byte stuffing.
+* frame_size is updated with the new size of the created frame;
 */
-void buildDataFrame(char** frame, char address_field, char control_field, char* data, int data_size);
+void buildDataFrame(char **frame, char *data, int data_size, int *frame_size);
 
 /**
-* Does byte stuffing on data.
-* data_size is updated as reallocs are made.
+* Does byte stuffing on frame.
+* frame_size is updated as reallocs are made.
 */
-void byteStuffing(char** data, int* data_size);
+void byteStuffing(char **frame, int *frame_size);
 
 /**
-* Does byte unstuffing on data.
-* data_size is updates as reallocs are made.
+* Does byte unstuffing on frame.
+* frame_size is updates as reallocs are made.
 */
-void byteUnstuffing(char** data, int* data_size);
+void byteUnstuffing(char **frame, int *frame_size);
+
+/**
+ * Returns the Address that should be used on a control frame.
+ * caller - Who called the function: SENDER or RECEIVER
+ * Returns: The address that should be used.
+ * */
+unsigned char getAddress(int caller, char control_field);
 
 /**
 * Inserts a value at a given position of the array.
 * Increments array_size by 1;
 */
-void insertValueAt(char value, char* array, int index, int *array_size);
-
-/**
-* Removes a value at a given position of the array.
-* Decrements array_size by 1;
-*/
-void removeValueAt(char* array, int index, int *array_size);
+void insertValueAt(char value, char *array, int index, int *array_size);
 
 /**
 * Opens/establishes the connection.
 * caller - Who called the function: SENDER or RECEIVER
 * Return: file descriptor;
 */
-int llopen(char* port, int caller);
-
-/**
-* Opens/establish the connection.
-* when caller is SENDER
-* Return: file descriptor;
-*/
-int llopenSender(int sp_fd);
+int llopen(char *port, int caller);
 
 /**
 * Opens/establish the connection.
@@ -66,10 +65,23 @@ int llopenSender(int sp_fd);
 int llopenReceiver(int fileDescriptor);
 
 /**
+* Opens/establish the connection.
+* when caller is SENDER
+* Return: file descriptor;
+*/
+int llopenSender(int sp_fd);
+
+/**
+* Removes a value at a given position of the array.
+* Decrements array_size by 1;
+*/
+void removeValueAt(char *array, int index, int *array_size);
+
+/**
 * Builds the data frame.
 * Sends the data frame until Receiver Ready or MAX_TRIES are achieved.
 */
-int llwrite(int sp_fd, char* data, int data_size);
+int llwrite(int sp_fd, char *data, int data_size);
 
 /**
 * Closes the connection.
